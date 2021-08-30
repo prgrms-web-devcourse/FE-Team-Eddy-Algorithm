0c636a61f481a32e832e4b3c18f5f608510a3ad7
HongJungKim-dev <hongjung.kim.dev@gmail.com>
2021-08-30 12:31:56 +0900

    refactor: 함수를 분리합니다.

--- week4/solution.js
+++ week4/solution.js
@@ -1,38 +1,54 @@
-function solution(table, languages, preference) {
+const calculateScore = (languages, table, 
+        preference, jobs, scores) => {
     const languagePreferenceZip = new Map();
-    table.sort();
+    const languageTable = [];
 
-    const jobs = new Map();
-    const newTable = [];
-    languages.map((language, index) => languagePreferenceZip.set(language, preference[index]))
+    languages.map((language, index) => languagePreferenceZip.set(language, preference[index]));
+
     table.forEach((column, index) => {
         const [job, ...languages] = column.split(' ');
-        newTable.push({
+
+        languageTable.push({
             ...languages
         });
-        jobs.set(index, job);
-    })
-    const scores = [];
-    
-    newTable.forEach((table) => {
-        let score = 0;
-        for(const [key, value] of Object.entries(table)){
 
+        jobs.set(index, job);
+    });
+
+    languageTable.forEach((table) => {
+        let score = 0;
+
+        for(const [key, value] of Object.entries(table)){
             if(languagePreferenceZip.get(value)){
                 score += languagePreferenceZip.get(value) * (jobs.size - key);
             }
         }
-        scores.push(score);
-    })
 
+        scores.push(score);
+    });
+}
+
+const getMaxScoreIndex = (scores) => {
     let maxScore = 0;
     let maxScoreIndex = 0;
+
     scores.forEach((score, index) => {
         if(maxScore < score){
             maxScore = score
             maxScoreIndex = index;
         }
-    })
+    });
+    
+    return maxScoreIndex;
+}
 
-    return jobs.get(maxScoreIndex);
+const solution = (table, languages, preference) => {
+    const copiedTable = table.slice();
+    copiedTable.sort();
+
+    const scores = [];
+    const jobs = new Map();
+    calculateScore(languages, copiedTable, preference, jobs, scores);
+
+    return jobs.get(getMaxScoreIndex(scores));
 }
