76b78d7252c4363e406ca5e93cf956a28fa7f9f6
HongJungKim-dev <hongjung.kim.dev@gmail.com>
2021-09-06 17:05:02 +0900

    feat: 하드코딩한것을 개선

--- week5/solution.js
+++ week5/solution.js
@@ -1,11 +1,24 @@
 const solution = (wordsStr) => {
-    const alphabet = { 'A': 0, 'E': 1, 'I': '2', 'O':3, 'U': 4 }
-    const increasing = [781, 156, 31, 6, 1]
-    const words = wordsStr.split('')
-
-    let order = 0
-    words.forEach((word, index) => {
-        order += alphabet[word] * increasing[index] + 1
+    const orderRules = [0, 0, 0, 0, 0];
+    let accumulator = 0;
+    const maxWordLength = 5;
+    const blankCase = 1;
+    const alphabet = { 'A': 0, 'E': 1, 'I': '2', 'O':3, 'U': 4 };
+    
+    const words = wordsStr.split('');
+    
+    orderRules.forEach((rule, index) => {
+        orderRules[index] = accumulator * maxWordLength + blankCase;
+        accumulator = orderRules[index];
     })
-    return order
+    
+    orderRules.reverse();
+
+    let order = 0;
+    
+    words.forEach((word, index) => {
+        order += alphabet[word] * orderRules[index] + 1;
+    })
+    
+    return order;
 }
