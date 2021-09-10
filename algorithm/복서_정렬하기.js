c6ae430e7fce7062c08288d2d282c5c481bac155
Jeongs <kimm.jeongs@gmail.com>
2021-09-11 01:08:18 +0900

    ♻️ Refactor: sorting boxers

--- algorithm/복서_정렬하기.js
+++ algorithm/복서_정렬하기.js
@@ -1,52 +1,46 @@
 function solution(weights, head2head) {
-  // 승률 높은 복서가 앞.
-  // 승률이 동일할 때 몸무게 무거운 복서 이긴횟수 높은 애가 앞
-  // 앞에것도 전부 동일하면 본인 몸무게가 무거운 애가 앞
-  // 나머지도 동일할 땐 번호가 작은게 앞
+  // boxers = [인덱스, 몸무게, 승률, 무거운 상대 이긴 횟수]
+  const boxers = []
+  const numberOfBoxers = weights.length
 
-  // winningRate = [인덱스, 몸무게, 승률, 무거운 상대 이긴 횟수]
-  const winningRate = []
-  const len = weights.length
-
-  for (let i = 0; i < len; i++) {
-    const main = weights[i]
+  for (let i = 0; i < numberOfBoxers; i++) {
+    const weight = weights[i]
     let totalPlayTimes = 0
     let countWin = 0
-    let GetWinWithHeavy = 0
+    let getWinWithHeavy = 0
 
-    for (let j = 0; j < len; j++) {
+    for (let j = 0; j < numberOfBoxers; j++) {
       const result = head2head[i][j]
       if (result !== 'N') totalPlayTimes++
       if (result === 'W') {
         countWin++
-        main < weights[j] && GetWinWithHeavy++
+        weight < weights[j] && getWinWithHeavy++
       }
     }
-    winningRate.push([
+    boxers.push([
       i + 1,
-      main,
+      weight,
       totalPlayTimes === 0 ? 0 : countWin / totalPlayTimes,
-      GetWinWithHeavy,
+      getWinWithHeavy,
     ])
   }
 
-  winningRate.sort((a, b) => {
-    const [aIndex, aWeight, aScore, aWinHeavyTimes] = a
-    const [bIndex, bWeight, bScore, bWinHeavyTimes] = b
+  return boxers
+    .sort((a, b) => {
+      const [aIndex, aWeight, aScore, aWinHeavyTimes] = a
+      const [bIndex, bWeight, bScore, bWinHeavyTimes] = b
 
-    if (aScore > bScore) return -1
-    if (aScore === bScore) {
-      if (aWinHeavyTimes > bWinHeavyTimes) return -1
-      if (aWinHeavyTimes === bWinHeavyTimes) {
-        if (aWeight > bWeight) return -1
-        if (aWeight === bWeight) {
-          if (aIndex > bIndex) return 1
-        }
-      }
-    }
-  })
-
-  return winningRate.map((result) => result[0])
+      // 뺀 값이 양수면 뒤로
+      if (aScore !== bScore) return bScore - aScore // 승률 높은 애가 앞
+      if (aWinHeavyTimes !== bWinHeavyTimes)
+        // 몸무게 무거운 복서 이긴횟수 높은 애가 앞
+        return bWinHeavyTimes - aWinHeavyTimes
+      // 본인 몸무게가 무거운 애가 앞
+      if (aWeight !== bWeight) return bWeight - aWeight
+      // 인덱스 숫자가 낮은 애가 앞
+      else return aIndex - bIndex
+    })
+    .map((result) => result[0])
 }
 
 const weights = [50, 82, 75, 120]
