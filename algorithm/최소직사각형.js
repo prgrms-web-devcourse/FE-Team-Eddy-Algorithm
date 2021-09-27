a4b883c6362706af9bcf9ebcacba1f5ba6a45a0b
AlangGY <rlaangh77@naver.com>
2021-09-28 02:48:12 +0900

    최소 직사각형 prettier

--- algorithm/최소직사각형.js
+++ algorithm/최소직사각형.js
@@ -1,8 +1,9 @@
 function solution(sizes) {
   return sizes
-      .map(size=>size.sort((a,b)=>a-b))
-      .reduce(([width,height],size)=>
-              [size[0] > width ? size[0]: width , size[1] > height ? size[1] : height]
-             )
-      .reduce((acc,length)=>acc*length)
+    .map((size) => size.sort((a, b) => a - b))
+    .reduce(([width, height], size) => [
+      size[0] > width ? size[0] : width,
+      size[1] > height ? size[1] : height,
+    ])
+    .reduce((acc, length) => acc * length);
 }
