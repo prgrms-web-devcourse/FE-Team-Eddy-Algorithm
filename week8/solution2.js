5d5b7325622244e4f618ecf6fb8789321b376728
HongJungKim-dev <hongjung.kim.dev@gmail.com>
2021-10-05 09:19:30 +0900

    refactor: 더 이상 대조할 필요없는 요소를 삭제하여 최적화

--- week8/solution2.js
+++ week8/solution2.js
@@ -4,12 +4,16 @@ function solution(lottos, winNums) {
     const unvisibleNumsLength = lottos.length - visibleNums.length;
     const visibleNumsAscending = visibleNums.sort((a, b) => (a - b));
     let correct = 0;
-    
+    const winNumsLength = winNums.length;
+
     for (const visibleNum of visibleNumsAscending) {
         for (const winNum of winNumsAscending) {
             if (visibleNum === winNum) {
                 correct += 1;
+                winNumsAscending.shift();
+                break;
             } else if (visibleNum < winNum) {
+                winNumsAscending.shift();
                 break;
             }
         }
@@ -17,7 +21,7 @@ function solution(lottos, winNums) {
 
     const ectCaseRank = 6;
     const maxCase = correct + unvisibleNumsLength;
-    const winNumsLength = winNums.length;
+    
     const maxRank = maxCase <= 1 ? ectCaseRank : winNumsLength - maxCase + 1;
     const minRank = correct <= 1 ? ectCaseRank : winNumsLength - correct + 1;
 
