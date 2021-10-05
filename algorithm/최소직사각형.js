47874f9f93509115d63ba5e2ebeff2283c6214be
Alang <75013334+AlangGY@users.noreply.github.com>
2021-10-05 15:41:36 +0900

    fix : 최소직사각형 refactor

--- algorithm/최소직사각형.js
+++ algorithm/최소직사각형.js
@@ -1,9 +1,9 @@
 function solution(sizes) {
   return sizes
     .map((size) => size.sort((a, b) => a - b))
-    .reduce(([width, height], size) => [
-      size[0] > width ? size[0] : width,
-      size[1] > height ? size[1] : height,
+    .reduce(([width, height], [maxWidth,maxHeight]) => [
+      maxWidth > width ? maxWidth : width,
+      maxHeight > height ? maxHeight : height,
     ])
-    .reduce((acc, length) => acc * length);
+    .reduce((maxWidth, maxHeight) => maxWidth * maxHeight);
 }
