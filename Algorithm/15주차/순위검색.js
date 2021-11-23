0e3c598e774c6c503ee6d566a790baa5d8c4a266
khw970421 <khw970421@kakao.com>
2021-11-23 15:04:15 +0900

    commit 15

---
+++ Algorithm/15주차/순위검색.js
@@ -0,0 +1,24 @@
+function solution(info, query) {
+  var answer = [];
+  query.forEach((str) => {
+    const [lang, jobGroup, experience, soulFoodAndScore] = str.split(" and ");
+    const [soulFood, score] = soulFoodAndScore.split(" ");
+    let count = 0;
+    info.forEach((infoStr) => {
+      const [infoLang, infoJobGroup, infoExperience, infoSoulFood, infoScore] =
+        infoStr.split(" ");
+
+      if (
+        (lang === "-" || lang === infoLang) &&
+        (jobGroup === "-" || jobGroup === infoJobGroup) &&
+        (experience === "-" || experience === infoExperience) &&
+        (soulFood === "-" || soulFood === infoSoulFood) &&
+        Number(score) <= Number(infoScore)
+      ) {
+        count++;
+      }
+    });
+    answer.push(count);
+  });
+  return answer;
+}
