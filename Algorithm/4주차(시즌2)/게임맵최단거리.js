657ab11914151942d6312348c50eb54a3cff1c6c
khw970421 <khw970421@kakao.com>
2022-01-22 16:19:48 +0900

    💄 style : 코드 가독성 높이기

--- Algorithm/4주차(시즌2)/게임맵최단거리.js
+++ Algorithm/4주차(시즌2)/게임맵최단거리.js
@@ -1,43 +1,40 @@
 function solution(maps) {
-  let site = [0, 0];
+  // 방문을 체크하는 visited와 방문 할 위치를 queue로 파악
   const visited = [0];
-  //방문 할 수 있는 곳들의 모임
   let queue = [[0, 0]];
 
   while (queue.length) {
     const [row, column] = queue.shift();
-    if (
-      maps[row][column + 1] === 1 &&
-      !visited[row * maps[0].length + column + 1]
-    ) {
-      visited[row * maps[0].length + column + 1] =
-        visited[row * maps[0].length + column] + 1;
+    const cur = row * maps[0].length + column;
+    // 상하좌우에 대한 이동이 가능체크
+    if (maps[row][column + 1] === 1 && !visited[cur + 1]) {
+      visited[cur + 1] = visited[cur] + 1;
       queue.push([row, column + 1]);
     }
-    if (
-      maps[row][column - 1] === 1 &&
-      !visited[row * maps[0].length + column - 1]
-    ) {
-      visited[row * maps[0].length + column - 1] =
-        visited[row * maps[0].length + column] + 1;
+    if (maps[row][column - 1] === 1 && !visited[cur - 1]) {
+      visited[cur - 1] = visited[cur] + 1;
       queue.push([row, column - 1]);
     }
-    if (maps[row - 1] !== undefined && maps[row - 1][column] === 1) {
-      if (!visited[(row - 1) * maps[0].length + column]) {
-        visited[(row - 1) * maps[0].length + column] =
-          visited[row * maps[0].length + column] + 1;
+    //행은 영역을 넘어가는 부분에 대한 체크를 미리  : maps[undefined][column]으로 접근은 에러 발생
+    if (maps[row - 1] !== undefined) {
+      if (maps[row - 1][column] === 1 && !visited[cur - maps[0].length]) {
+        visited[cur - maps[0].length] = visited[cur] + 1;   
         queue.push([row - 1, column]);
       }
     }
-    if (maps[row + 1] !== undefined && maps[row + 1][column] === 1) {
-      if (!visited[(row + 1) * maps[0].length + column]) {
-        visited[(row + 1) * maps[0].length + column] =
-          visited[row * maps[0].length + column] + 1;
+    if (maps[row + 1] !== undefined) {
+      if (maps[row + 1][column] === 1 && !visited[cur + maps[0].length]) {
+        visited[cur + maps[0].length] = visited[cur] + 1;   
         queue.push([row + 1, column]);
       }
     }
   }
+
+  //마지막 값이 어떠한 값이 존재한다면 접근에 성공 / 값이 없다면 도달 실패
   return visited[maps[0].length * maps.length - 1] === undefined
     ? -1
     : visited[visited.length - 1] + 1;
 }
+
+
+// visited[cur - maps[0].length] = visited[cur] + 1; 는 visited[(row - 1) * maps[0].length + column]와 같다. 
