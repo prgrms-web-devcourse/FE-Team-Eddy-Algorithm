5c4dc57a5414f07085644838ab9f9559336ab74f
khw970421 <khw970421@kakao.com>
2022-01-22 13:53:36 +0900

    ✨ 짝지어 제거하기

---
+++ Algorithm/4주차(시즌2)/짝지어제거하기.js
@@ -0,0 +1,15 @@
+function solution(s)
+{
+    if(s.length%2===1)
+        return 0
+    const arr = []
+   s.split('').map((str)=>{
+       if(arr.length === 0)
+           arr.push(str)
+       else if(arr[arr.length-1] === str)
+           arr.pop()
+       else
+           arr.push(str)
+   })
+   return arr.length === 0 ? 1 : 0
+}
