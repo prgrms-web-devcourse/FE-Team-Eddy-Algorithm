d9ae25b1606e64e096936bf604eadc4af3dba4a7
khj <hongjung.kim.dev@gmail.com>
2022-02-16 21:57:23 +0900

    refactor: 의미있는 여러 함수로 분리

--- season2/week6/getReportResults.js
+++ season2/week6/getReportResults.js
@@ -1,4 +1,46 @@
-function solution(ids, reports, stoppedCount) {
+/*
+  Todo : users의 값이 함수호출마다 변하는데 함수형으로 개선할 수 없을까?
+*/
+const calculateStoppedIdsNum = (ids, users, stoppedCount) => {
+  const answer = [];
+
+  for (const id of ids) {
+    const { reportIds } = users[id];
+    const stoppedIds = reportIds.filter(
+      (id) => users[id].stoppedCount >= stoppedCount
+    );
+
+    answer.push(stoppedIds.length);
+  }
+
+  return answer;
+};
+
+const calculateStoppedCount = (ids, users) => {
+  for (const id of ids) {
+    const { reportIds } = users[id];
+
+    for (const reportId of reportIds) {
+      if (!users[reportId]) {
+        continue;
+      }
+
+      users[reportId].stoppedCount += 1;
+    }
+  }
+};
+
+const setReportIds = (reports, users) => {
+  for (const report of reports) {
+    const [reporterId, targetId] = report.split(" ");
+
+    users[reporterId].reportIds = [
+      ...new Set([...users[reporterId].reportIds, targetId]),
+    ];
+  }
+};
+
+const initializeUsers = (ids) => {
   const users = {};
 
   for (const id of ids) {
@@ -11,35 +53,15 @@ function solution(ids, reports, stoppedCount) {
       });
   }
 
-  for (const report of reports) {
-    const [reporterId, targetId] = report.split(" ");
+  return users;
+};
 
-    users[reporterId].reportIds = [
-      ...new Set([...users[reporterId].reportIds, targetId]),
-    ];
-  }
+const solution = (ids, reports, stoppedCount) => {
+  const users = initializeUsers(ids);
 
-  for (const id of ids) {
-    const { reportIds } = users[id];
+  setReportIds(reports, users);
 
-    for (const reportId of reportIds) {
-      if (!users[reportId]) {
-        continue;
-      }
+  calculateStoppedCount(ids, users);
 
-      users[reportId].stoppedCount += 1;
-    }
-  }
-
-  const answer = [];
-  for (const id of ids) {
-    const { reportIds } = users[id];
-    const stoppedIds = reportIds.filter(
-      (id) => users[id].stoppedCount >= stoppedCount
-    );
-
-    answer.push(stoppedIds.length);
-  }
-
-  return answer;
-}
+  return calculateStoppedIdsNum(ids, users, stoppedCount);
+};
