2b2c58d4fb627ad2a2d3a125124f03fcdb545465
khw970421 <khw970421@kakao.com>
2022-02-26 16:50:33 +0900

    성공

--- Algorithm/7주차(시즌2)/숫자의 표현.js
+++ Algorithm/7주차(시즌2)/숫자의 표현.js
@@ -1,6 +1,5 @@
 function solution(n) {
     let answer = 0;
-    // 근의공식 활용
     for(let i=Math.ceil(n/2);i>=1;i--){
         const startValue  = (1+Math.sqrt(1+4*(Math.pow(i,2)+i-2*n)))/2
         answer += Number.isInteger(startValue) ? 1 : 0
